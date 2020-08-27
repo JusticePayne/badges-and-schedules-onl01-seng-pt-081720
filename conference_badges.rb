c0024f850e382927bb3c6a0233294bfa3e1548f7
Justice Payne <spikepayne@yahoo.com>
2020-08-27 15:25:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(people)
   newarray = []
   counter = 1
   people.each do |name|
-    newarray.push "Hello, ${name}!, you'll be assigned to room #{counter}!"
+    newarray.push "Hello, #{$}{name}!, you'll be assigned to room #{counter}!"
     counter += 1
   end
   return newarray
