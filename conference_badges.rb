3bf6b82b246f0cb22d2792cc823d42b7913d1a19
Justice Payne <spikepayne@yahoo.com>
2020-08-27 14:32:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus" "Matz"]
 
 def badge_maker(people)
-  return "Hello my name is #{people}."
+  return "Hello, my name is #{people}."
 end
