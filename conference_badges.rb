f9559fd1ee8330ff63ca7ac66968a634260e2535
Justice Payne <spikepayne@yahoo.com>
2020-08-27 15:17:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,3 +12,17 @@ def batch_badge_creator(people)
   end
   return nuarray
 end
+
+
+def room_assignments.(people)
+  newarray = []
+  counter 1
+  people.each do |name|
+    newarray.push "Hello, ${name}!, you will be assigned to room #{counter}"
+    counter += 1
+  end
+  newarray 
+end
+
+
+    
