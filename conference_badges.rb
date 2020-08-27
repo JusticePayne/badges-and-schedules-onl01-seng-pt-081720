66e35305bb7fbb6e3fe8b3c912a5f9e6c6906530
Justice Payne <spikepayne@yahoo.com>
2020-08-27 14:41:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,9 @@
 people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus" "Matz"]
 
-def badge_maker(people)
-  return "Hello, my name is #{people}."
+def batch_badge_creator(people)
+  nuarray = []
+  people.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
+  end
+  return nuarray
 end
