516ccfd000a6ed2345a72a1c9ceaa5d0a308fb3c
Justice Payne <spikepayne@yahoo.com>
2020-08-27 14:43:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus" "Matz"]
 def badge_maker(people)
   return "Hello, my name is #{name}."
 end
-  
+
 
 def batch_badge_creator(people)
   nuarray = []
